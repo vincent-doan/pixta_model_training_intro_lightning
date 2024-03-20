19fc0ae8a02180426cb586b0e4726fca73928142
Vincent-Doan <vinh.dt210940@sis.hust.edu.vn>
2024-03-20 16:55:02 +0000

    dtv/feat(loss) update loss to allow variable output size

--- loss.py
+++ loss.py
@@ -2,15 +2,16 @@ import torch
 from torch import nn
 
 class WeightedBCELoss(nn.Module):
-    def __init__(self, scale:int, reduction:str):
+    def __init__(self, output_size:int, scale:int, reduction:str):
         super(WeightedBCELoss, self).__init__()
+        self.output_size = output_size
         self.scale = scale
         self.reduction = reduction
 
     def forward(self, inputs, targets):
         batch_size = inputs.size(0)
         device = inputs.device
-        loss = nn.BCEWithLogitsLoss(reduction='sum', pos_weight=torch.ones((batch_size, 20), device=device)*self.scale)(inputs, targets)
+        loss = nn.BCEWithLogitsLoss(reduction='sum', pos_weight=torch.ones((batch_size, self.output_size), device=device)*self.scale)(inputs, targets)
         if self.reduction == 'sum':
             return torch.sum(loss)
         elif self.reduction == 'mean':
